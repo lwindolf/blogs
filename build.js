5ebd53d9c870d545f229b8a1c85a6aba65494c83
Lars Windolf <lars.windolf@gmx.de>
2025-05-31 23:44:48 +0200

    Update build.js

--- build.js
+++ build.js
@@ -22,7 +22,7 @@ if (!fs.existsSync(outputDir)) {
         {
                 title       : "Liferea Development Blog",
                 description : "Latest posts from the Liferea blog",
-                favicon     : "https://lzone.de/liferea/favicon.ico",
+                favicon     : "https://lzone.de/liferea/favicon.svg",
                 link        : "https://lzone.de/liferea/blog",
                 input       : path.join(import.meta.dirname, 'Blogs', 'Liferea'),
                 output      : path.join(outputDir, 'liferea.xml')
